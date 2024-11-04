5e6ddd31e290d0b88c9104aa7cc1913229243fd5
Abhipsha BHATTA <bhattaabhipsha@gmail.com>
2024-11-04 15:08:46 +0100

    Objects

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log("test")
+console.log("test from Node");
+
+const userOne = {
+    firstName: "Bog",
+    age: 27,
+    admin: false
+};
+
+userOne.age = 30;
+
+//console.log(userOne);
+
+
+const sports = ["tennis", "golf", "volleyball"];
+sports.push("test");
+
+const foundSport = sports.indexOf("test")
+console.log(foundSport);
+
+
+
